764be09bdafcc9111e7f39b1e05454bd113f1099
ken <ken@126.com>
2017-11-21 13:56:14 +0800

    modify udp client to bind src port

--- udpcliserv/udpcli01.c
+++ udpcliserv/udpcli01.c
@@ -5,17 +5,29 @@ main(int argc, char **argv)
 {
 	int					sockfd;
 	struct sockaddr_in	servaddr;
+	struct sockaddr_in	srcaddr;
 
-	if (argc != 2)
-		err_quit("usage: udpcli <IPaddress>");
+	if (argc != 3)
+		err_quit("usage: udpcli <IPaddress> <port>");
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERV_PORT);
 	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
+
+	bzero(&srcaddr, sizeof(srcaddr));
+	srcaddr.sin_family = AF_INET;
+	srcaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	srcaddr.sin_port = htons(atoi(argv[2]));
+
+
 	sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
 
+	if (bind(sockfd, (struct sockaddr *) &srcaddr, sizeof(srcaddr)) < 0) {
+		perror("bind");
+		exit(1);
+	}
 	dg_cli(stdin, sockfd, (SA *) &servaddr, sizeof(servaddr));
 
 	exit(0);
