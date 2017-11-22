8f156c7624ccd7ef06726aae8be9258ae63eff83
jianmingfan <jianmingfan@126.com>
2017-11-22 11:04:30 +0800

    udpserver01 can bind to address now

--- udpcliserv/udpserv01.c
+++ udpcliserv/udpserv01.c
@@ -5,13 +5,19 @@ main(int argc, char **argv)
 {
 	int					sockfd;
 	struct sockaddr_in	servaddr, cliaddr;
-
+	unsigned int srcaddr;
 	sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
+	char *host = argv[1];
+	if (argc != 2) {
+		printf("input host addr to bind\n");
+		return -1;
+	}
 
+	inet_pton(AF_INET, host, &srcaddr);
+	printf("src addr net order is %x\n", srcaddr);
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
-	//servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_addr.s_addr = htonl(0x64400001);
+	servaddr.sin_addr.s_addr = srcaddr;
 	servaddr.sin_port        = htons(SERV_PORT);
 
 	Bind(sockfd, (SA *) &servaddr, sizeof(servaddr));
